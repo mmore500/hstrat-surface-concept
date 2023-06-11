3252025011b1c85df87d43b384c78bd102a2477d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-11 13:00:36 -0700

    Switch to zero-based Hanoi sequence variant

--- pylib/hanoi/get_hanoi_value_at_index.py
+++ pylib/hanoi/get_hanoi_value_at_index.py
@@ -1,4 +1,4 @@
 def get_hanoi_value_at_index(n: int) -> int:
     # https://oeis.org/A00151
     n += 1
-    return (n&-n).bit_length()
+    return (n&-n).bit_length() - 1
--- pylib/hanoi/get_index_of_hanoi_value_nth_incidence.py
+++ pylib/hanoi/get_index_of_hanoi_value_nth_incidence.py
@@ -1,5 +1,4 @@
 def get_index_of_hanoi_value_nth_incidence(value: int, i: int) -> int:
-    value -= 1
     offset = 2 ** value - 1
     cadence = 2 ** (value + 1)
     return offset + cadence * i
