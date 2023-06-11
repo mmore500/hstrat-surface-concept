fca79a54f5c08a3c69c952898d45283894fa79a3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-11 13:00:54 -0700

    Fixup docstrings, signatures

--- pylib/hanoi/get_hanoi_value_incidence_at_index.py
+++ pylib/hanoi/get_hanoi_value_incidence_at_index.py
@@ -1,5 +1,9 @@
 from .get_hanoi_value_at_index import get_hanoi_value_at_index
 
 def get_hanoi_value_incidence_at_index(n: int) -> int:
-    """How many times has the hanoi value at index n already been encountered?"""
+    """How many times has the hanoi value at index n already been encountered?
+
+    Assumes zero-indexing convention. See `get_hanoi_value_at_index` for notes
+    on zero-based variant of Hanoi sequence used.
+    """
     return n // 2 ** (get_hanoi_value_at_index(n) + 1)
--- pylib/hanoi/get_index_of_hanoi_value_nth_incidence.py
+++ pylib/hanoi/get_index_of_hanoi_value_nth_incidence.py
@@ -1,4 +1,10 @@
-def get_index_of_hanoi_value_nth_incidence(value: int, i: int) -> int:
+def get_index_of_hanoi_value_nth_incidence(value: int, n: int) -> int:
+    """At what index does the nth incidence of a given value occur within the
+    Hanoi sequence?
+
+    Assumes zero-indexing convention. See `get_hanoi_value_at_index` for notes
+    on zero-based variant of Hanoi sequence used.
+    """
     offset = 2 ** value - 1
     cadence = 2 ** (value + 1)
-    return offset + cadence * i
+    return offset + cadence * n
