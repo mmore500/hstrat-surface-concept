f1869707b44c40adbe67337e6828e12f72128c61
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-12 09:26:13 -0700

    Impl, test mathematical modulo function

--- pylib/__init__.py
+++ pylib/__init__.py
@@ -1,6 +1,7 @@
 from .bit_ceil import bit_ceil
 from .bit_floor import bit_floor
 from .get_powersof2triangle_val_at_index import get_powersof2triangle_val_at_index
+from .modulo import modulo
 from . import hanoi
 from . import longevity_ordering
 
@@ -8,6 +9,7 @@ __all__ = [
     "bit_ceil",
     "bit_floor",
     "get_powersof2triangle_val_at_index",
+    "modulo",
     "hanoi",
     "longevity_ordering",
 ]
