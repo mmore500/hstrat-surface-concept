7aedce3375740dace8c675d3ad5f01d63aad1e4c
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-11 19:44:45 -0700

    Impl, test get_longevity_index_of_mapped_position

--- pylib/longevity_ordering/__init__.py
+++ pylib/longevity_ordering/__init__.py
@@ -1,3 +1,6 @@
+from .get_longevity_index_of_mapped_position import (
+    get_longevity_index_of_mapped_position,
+)
 from .get_longevity_level_of_index import get_longevity_level_of_index
 from .get_longevity_level_of_mapped_position import get_longevity_level_of_mapped_position
 from .get_longevity_offset_of_level import get_longevity_offset_of_level
@@ -6,6 +9,7 @@ from .get_longevity_ordered_position_of_index import (
 )
 
 __all__ = [
+    "get_longevity_index_of_mapped_position",
     "get_longevity_level_of_index",
     "get_longevity_level_of_mapped_position",
     "get_longevity_offset_of_level",
---
+++ pylib/longevity_ordering/get_longevity_index_of_mapped_position.py
@@ -0,0 +1,30 @@
+from .get_longevity_level_of_mapped_position import (
+    get_longevity_level_of_mapped_position,
+)
+from .get_longevity_offset_of_level import get_longevity_offset_of_level
+
+
+def get_longevity_index_of_mapped_position(
+    mapped_position: int,
+    num_positions: int,
+) -> int:
+    longevity_level = get_longevity_level_of_mapped_position(
+        mapped_position,
+        num_positions,
+    )
+
+    offset = get_longevity_offset_of_level(longevity_level, num_positions)
+    spacing = offset * 2
+
+    assert spacing == 0 or (mapped_position - offset) % spacing == 0
+    position_within_level = (mapped_position - offset) // spacing if spacing else 0
+
+    # level -> num_positions_at_lower_levels
+    # 0 -> 0
+    # 1 -> 1
+    # 2 -> 2, etc.
+    # 3 -> 4, etc.
+    num_positions_at_lower_levels = 2 ** (longevity_level - 1) if longevity_level else 0
+
+    index = position_within_level + num_positions_at_lower_levels
+    return index
