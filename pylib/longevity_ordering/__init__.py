90546ee0a82ea6ada542f4cf737ba83ec0511e11
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-11 19:22:24 -0700

    Impl, test get_longevity_level_of_mapped_position

--- pylib/longevity_ordering/__init__.py
+++ pylib/longevity_ordering/__init__.py
@@ -1,4 +1,5 @@
 from .get_longevity_level_of_index import get_longevity_level_of_index
+from .get_longevity_level_of_mapped_position import get_longevity_level_of_mapped_position
 from .get_longevity_offset_of_level import get_longevity_offset_of_level
 from .get_longevity_ordered_position_of_index import (
     get_longevity_ordered_position_of_index,
@@ -6,6 +7,7 @@ from .get_longevity_ordered_position_of_index import (
 
 __all__ = [
     "get_longevity_level_of_index",
+    "get_longevity_level_of_mapped_position",
     "get_longevity_offset_of_level",
     "get_longevity_ordered_position_of_index",
 ]
