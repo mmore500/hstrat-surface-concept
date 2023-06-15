6418e4c938fa1c7f7fde63e5d6ea6837ce8efdb4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 11:50:37 -0700

    Delete dead code

--- pylib/get_longeveity_ordered_position.py
+++
@@ -1,8 +0,0 @@
-def get_longeveity_ordered_position(i: int, num_items: int) -> int:
-    longeveity_depth = (window_idx + 1).bit_length() - 1
-    longeveity_offset = num_windows // 2**longeveity_depth
-    longeveity_spacing = longeveity_offset * 2
-    longeveity_ordered_window_idx = (
-        longeveity_offset
-        + longeveity_spacing * pylib.get_powersof2triangle_val_at_index(window_idx)
-    )
