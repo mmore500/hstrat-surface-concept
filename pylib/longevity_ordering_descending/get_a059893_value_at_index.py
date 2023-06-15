27f15fb5c68efd3934bdc6e5554bc7305ce7a48e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 13:51:19 -0700

    Optimize a059893 impl

--- pylib/longevity_ordering_descending/get_a059893_value_at_index.py
+++ pylib/longevity_ordering_descending/get_a059893_value_at_index.py
@@ -1,8 +1,7 @@
-from ..bit_drop_msb import bit_drop_msb
 from ..bit_floor import bit_floor
 from ..bit_reverse import bit_reverse
 
 
 def get_a059893_value_at_index(n: int) -> int:
     # https://oeis.org/A059893
-    return int('1' + bin(n + 1)[3:][::-1], 2)
+    return bit_floor(n + 1) | (bit_reverse(n+1) >> 1)
