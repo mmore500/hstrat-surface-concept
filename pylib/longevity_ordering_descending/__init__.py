b5b60c90d16d2160b3d40a1b4a6e7589125cdabc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 14:10:04 -0700

    Implement, test get_a341916_value_at_index

--- pylib/longevity_ordering_descending/__init__.py
+++ pylib/longevity_ordering_descending/__init__.py
@@ -1,5 +1,7 @@
 from .get_a059893_value_at_index import get_a059893_value_at_index
+from .get_a341916_value_at_index import get_a341916_value_at_index
 
 __all__ = [
     "get_a059893_value_at_index",
+    "get_a341916_value_at_index",
 ]
