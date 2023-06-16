04110cdfc8c83fac3f147dd98e61c5fffd7c7fe0
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 22:11:58 -0700

    Impl, test longevity piecewise ascending inverse

---
+++ pylib/longevity_ordering_piecewise_ascending/get_longevity_index_of_mapped_position.py
@@ -0,0 +1,31 @@
+from .. import longevity_ordering_naive as lon
+from ..oeis import get_a030109_index_of_value
+from ..longevity_ordering_common import (
+    get_longevity_level_of_mapped_position,
+    get_longevity_num_positions_at_level,
+    get_longevity_num_positions_at_lower_levels,
+    get_longevity_offset_of_level,
+)
+
+
+def get_longevity_index_of_mapped_position(
+    mapped_position: int,
+    num_positions: int,
+) -> int:
+    longevity_level = get_longevity_level_of_mapped_position(
+        mapped_position,
+        num_positions,
+    )
+    naive_position_within_level = lon.get_longevity_position_within_level(
+        mapped_position,
+        num_positions,
+    )
+    index = (
+        get_a030109_index_of_value(
+            naive_position_within_level,
+            longevity_level,
+        ) + 1
+        if mapped_position
+        else 0
+    )
+    return index
