dfec83fdc29f7e20083de980ef61231618b3d32b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 23:43:57 -0700

    Impl, test get index of hanoi value next incidence

--- pylib/hanoi/__init__.py
+++ pylib/hanoi/__init__.py
@@ -5,6 +5,9 @@ from .get_hanoi_value_index_offset import get_hanoi_value_index_offset
 from .get_incidence_count_of_hanoi_value_through_index import (
     get_incidence_count_of_hanoi_value_through_index,
 )
+from .get_index_of_hanoi_value_next_incidence import (
+    get_index_of_hanoi_value_next_incidence,
+)
 from .get_index_of_hanoi_value_nth_incidence import (
     get_index_of_hanoi_value_nth_incidence,
 )
@@ -16,6 +19,7 @@ __all__ = [
     "get_hanoi_value_at_index",
     "get_hanoi_value_incidence_at_index",
     "get_incidence_count_of_hanoi_value_through_index",
+    "get_index_of_hanoi_value_next_incidence",
     "get_index_of_hanoi_value_nth_incidence",
     "get_max_hanoi_value_through_index",
 ]
