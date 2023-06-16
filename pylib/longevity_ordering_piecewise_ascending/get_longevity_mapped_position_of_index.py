18a54958d934bc8496d1a59c3b330c870fb293c4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-15 19:25:04 -0700

    Impl, test piecewise ascending mapping

---
+++ pylib/longevity_ordering_piecewise_ascending/get_longevity_mapped_position_of_index.py
@@ -0,0 +1,22 @@
+from ..oeis import get_a030109_value_at_index
+
+from ..longevity_ordering_common import (
+    get_longevity_level_of_index,
+    get_longevity_offset_of_level,
+)
+
+def get_longevity_mapped_position_of_index(
+    index: int,
+    num_indices: int,
+) -> int:
+    longevity_level = get_longevity_level_of_index(index)
+    position_within_level = (
+        get_a030109_value_at_index(index - 1)
+        if index
+        else 0
+    )
+
+    offset = get_longevity_offset_of_level(longevity_level, num_indices)
+    spacing = offset * 2
+    position = offset + spacing * position_within_level
+    return position
