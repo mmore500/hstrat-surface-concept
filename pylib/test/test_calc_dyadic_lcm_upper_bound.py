4d0eca2ae3fe1890983889fdb8a8482fd3b24627
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-20 00:21:33 -0700

    Test algorithm assumptions

--- pylib/test/test_calc_dyadic_lcm_upper_bound.py
+++ pylib/test/test_calc_dyadic_lcm_upper_bound.py
@@ -1,3 +1,4 @@
+from hstrat import _auxiliary_lib as hstrat_auxlib
 import numpy as np
 import pytest
 
@@ -33,3 +34,30 @@ def test_upper_bound_correctness():
             assert upper_bound >= true_lcm
             assert upper_bound % a == 0
             assert upper_bound % b == 0
+
+
+def test_site_selection_algorithm_assumption():
+    def pow2(n: int) -> int:
+        return 1 << n
+
+    for granularity in map(pow2, range(1, 65)):
+        for tour_size in map(pow2, range(2, 65)):
+            assert hstrat_auxlib.popcount(granularity) == 1
+            assert hstrat_auxlib.popcount(tour_size) == 1
+
+            if granularity >= tour_size // 4:
+                continue
+            for step in range(min(granularity, 1000)):
+                a = tour_size * (2 * granularity - step) / (2 * granularity)
+                b = (
+                    tour_size
+                    * (2 * granularity - step - 1)
+                    / (2 * granularity)
+                )
+                assert a.is_integer() and b.is_integer()
+                a, b = int(a), int(b)
+                assert a and b
+                assert (
+                    pylib.calc_dyadic_lcm_upper_bound(a, b)
+                    <= 2 * granularity * tour_size
+                )
