a6dc396cfcf05b4bf14fd028e8f8f1de715aaf95
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-22 20:05:17 -0700

    Hide tests and boilerplate by default

--- pylib/jupyter_hide_toggle.py
+++ pylib/jupyter_hide_toggle.py
@@ -1,21 +1,47 @@
 import random
 
-from IPython.display import HTML
+from IPython.display import HTML, Javascript
+
+def jupyter_hide_toggle(hide: bool = False):
+    if hide:
+        return _jupyter_hide_toggle_hide()
+    else:
+        return _jupyter_hide_toggle_show()
 
 
 # adapted from https://stackoverflow.com/a/52664156
-def jupyter_hide_toggle(for_next=False):
+def _jupyter_hide_toggle_hide():
+    js_code = """
+    function createToggleButton(cell) {
+        var toggle_button = $('<button>').text('Toggle Input').click(function () {
+            var input_area = cell.find('.input');
+            input_area.toggle();
+        });
+
+        // Append button to the cell's toolbar
+        cell.find('.input').before(toggle_button);
+
+        // Hide input area by default
+        cell.find('.input').hide();
+    }
+
+    var output_area = this;
+
+    // Find the parent cell element
+    var cell_element = output_area.element.parents('.cell');
+
+    // Create toggle button for this cell
+    createToggleButton(cell_element);
+    """
+
+    return Javascript(js_code)
+
+
+def _jupyter_hide_toggle_show():
     this_cell = """$('div.cell.code_cell.rendered.selected')"""
-    next_cell = this_cell + ".next()"
 
     toggle_text = "Toggle show/hide"  # text shown on toggle link
     target_cell = this_cell  # target cell to control with toggle
-    js_hide_current = ""  # bit of JS to permanently hide code in current cell (only when toggling next cell)
-
-    if for_next:
-        target_cell = next_cell
-        toggle_text += " next cell"
-        js_hide_current = this_cell + '.find("div.input").hide();'
 
     js_f_name = "code_toggle_{}".format(str(random.randint(1, 2**64)))
 
@@ -25,14 +51,12 @@ def jupyter_hide_toggle(for_next=False):
                 {cell_selector}.find('div.input').toggle();
             }}
 
-            {js_hide_current}
         </script>
 
         <a href="javascript:{f_name}()">{toggle_text}</a>
     """.format(
         f_name=js_f_name,
         cell_selector=target_cell,
-        js_hide_current=js_hide_current,
         toggle_text=toggle_text,
     )
 
