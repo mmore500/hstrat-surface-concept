cabed102831d553e76b5a23c2fd071a834fbf006
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-26 00:38:32 -0700

    Plan to use cooltuk ordering for term rung algo

--- hsurf/site_selection_strategy/site_selection_algorithms/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/__init__.py
@@ -1,13 +1,13 @@
 from . import (
+    hanoi_cooltuk_order_term_rungs_algo,
     hanoi_grundy_order_incidence_rungs_algo,
     hanoi_grundy_order_term_rungs_algo,
     hanoi_hadamard_order_site_rungs_algo,
-    hanoi_hadamard_order_term_rungs_algo,
 )
 
 __all__ = [
+    "hanoi_cooltuk_order_term_rungs_algo",
     "hanoi_grundy_order_incidence_rungs_algo",
     "hanoi_grundy_order_term_rungs_algo",
-    "hanoi_hadamard_order_term_rungs_algo",
     "hanoi_hadamard_order_site_rungs_algo",
 ]
