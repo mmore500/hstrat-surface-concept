70badb1b57c0d79d0ac0b7e57c1af3d8eb58e8bc
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-26 13:29:32 -0700

    Transfer, test is_2x_reservation_eligible

--- hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/__init__.py
@@ -4,8 +4,10 @@ from ._get_num_incidence_reservations_at_rank import (
 from ._get_num_sites_reserved_per_incidence_at_rank import (
     get_num_sites_reserved_per_incidence_at_rank,
 )
+from ._is_2x_reservation_eligible import is_2x_reservation_eligible
 
 __all__ = [
     "get_num_incidence_reservations_at_rank",
     "get_num_sites_reserved_per_incidence_at_rank",
+    "is_2x_reservation_eligible",
 ]
---
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/_is_2x_reservation_eligible.py
@@ -0,0 +1,20 @@
+from deprecated import deprecated
+
+from .....pylib import hanoi
+from ._get_num_sites_reserved_per_incidence_at_rank import (
+    get_num_sites_reserved_per_incidence_at_rank,
+)
+
+
+@deprecated()
+def is_2x_reservation_eligible(
+    hanoi_value: int, surface_size: int, rank: int
+) -> bool:
+    reservation_width = get_num_sites_reserved_per_incidence_at_rank(rank)
+    lb_inclusive = (
+        hanoi.get_max_hanoi_value_through_index(rank)
+        - reservation_width // 2
+        + 1
+    )
+    ub_exclusive = reservation_width // 2
+    return lb_inclusive <= hanoi_value < ub_exclusive
