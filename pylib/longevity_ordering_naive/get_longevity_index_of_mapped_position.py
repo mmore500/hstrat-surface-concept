2fd2420b55a93cb39e7c4a3b934c8a4f4687964e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-26 01:57:05 -0700

    Sub in efficient oeis bitmagic

--- pylib/longevity_ordering_naive/get_longevity_index_of_mapped_position.py
+++ pylib/longevity_ordering_naive/get_longevity_index_of_mapped_position.py
@@ -1,30 +1,11 @@
-from ..longevity_ordering_common import (
-    get_longevity_level_of_mapped_position,
-    get_longevity_num_positions_at_lower_levels,
-)
-from .get_longevity_position_within_level import (
-    get_longevity_position_within_level,
+from ..oeis import (
+    get_a025480_value_at_index,
 )
 
 
 def get_longevity_index_of_mapped_position(
-    mapped_position: int,
-    num_positions: int,
+    index: int, num_indices: int
 ) -> int:
-    position_within_level = get_longevity_position_within_level(
-        mapped_position,
-        num_positions,
-    )
-
-    longevity_level = get_longevity_level_of_mapped_position(
-        mapped_position,
-        num_positions,
-    )
-    num_positions_at_lower_levels = (
-        get_longevity_num_positions_at_lower_levels(
-            longevity_level,
-        )
-    )
-
-    index = position_within_level + num_positions_at_lower_levels
-    return index
+    assert num_indices.bit_count() == 1
+    grundy_offset = num_indices - 1
+    return get_a025480_value_at_index(grundy_offset + index)
