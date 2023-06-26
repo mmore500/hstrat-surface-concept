a9e3f175dcbbd9c7418d1a4bee6150fbbcadbf16
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-26 13:51:20 -0700

    Mark, explain deprecations

--- hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/_is_2x_reservation_eligible.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/_is_2x_reservation_eligible.py
@@ -6,7 +6,9 @@ from ._get_num_sites_reserved_per_incidence_at_rank import (
 )
 
 
-@deprecated()
+@deprecated(
+    reason="Redundant to other implementation logic; should consolidate."
+)
 def is_2x_reservation_eligible(
     hanoi_value: int, surface_size: int, rank: int
 ) -> bool:
--- hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/_iter_hanoi_invader_values.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/_iter_hanoi_invader_values.py
@@ -1,9 +1,12 @@
 import itertools as it
 import typing
 
+from deprecated import deprecated
+
 from .....pylib import bit_ceil
 
 
+@deprecated(reason="Uses generator. Should refactor to functional form.")
 def iter_hanoi_invader_values(hanoi_value: int) -> typing.Iterator[int]:
     for i in it.count():
         yield hanoi_value + bit_ceil(hanoi_value + 1) * 2**i
