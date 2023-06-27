b7adc40adaaad460ccbb414fd08b555aaa47efd9
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-26 20:25:15 -0700

    Transfer, test has_hanoi_value_filled_first_reservation_layer

--- hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/_impl/__init__.py
@@ -5,6 +5,9 @@ from ._get_num_sites_reserved_per_incidence_at_rank import (
     get_num_sites_reserved_per_incidence_at_rank,
 )
 from ._get_surface_rank_capacity import get_surface_rank_capacity
+from ._has_hanoi_value_filled_first_reservation_layer import (
+    has_hanoi_value_filled_first_reservation_layer,
+)
 from ._is_2x_reservation_eligible import is_2x_reservation_eligible
 from ._is_hanoi_invadable_and_uninvaded import is_hanoi_invadable_and_uninvaded
 from ._is_hanoi_invaded import is_hanoi_invaded
@@ -15,6 +18,7 @@ __all__ = [
     "get_num_incidence_reservations_at_rank",
     "get_num_sites_reserved_per_incidence_at_rank",
     "get_surface_rank_capacity",
+    "has_hanoi_value_filled_first_reservation_layer",
     "is_2x_reservation_eligible",
     "is_hanoi_invadable_and_uninvaded",
     "is_hanoi_invaded",
