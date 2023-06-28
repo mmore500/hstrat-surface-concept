671351cc9d4fb46c6e09c46acb30099e4386d547
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-06-28 07:47:51 -0400

    Move dep rank calc files to _scry/, smoke test

--- hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/hanoi_hadamard_order_site_rungs_algo/__init__.py
@@ -1,9 +1,11 @@
 """Transplanted from
 `05-fractional-incrementing-incidence-reservation-surface-with-safety-aligned-transition.ipynb`"""
 
-from ._calc_resident_deposition_rank import calc_resident_deposition_rank
 from ._enact import pick_deposition_site
-from ._iter_resident_deposition_ranks import iter_resident_deposition_ranks
+from ._scry import (
+    calc_resident_deposition_rank,
+    iter_resident_deposition_ranks,
+)
 
 __all__ = [
     "calc_resident_deposition_rank",
---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_hanoi_hadamard_order_site_rungs_algo/test_scry/test_calc_resident_deposition_rank.py
@@ -0,0 +1,6 @@
+from hsurf.hsurf import hanoi_hadamard_order_site_rungs_algo as algo
+
+
+def test_calc_resident_deposition_rank():
+    # just a smoke test
+    algo.calc_resident_deposition_rank
