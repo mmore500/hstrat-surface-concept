ec52957f5041dc82cc0da05df5f9e5e96709eb80
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-01 12:57:42 -0400

    Integration test hadamard/site calc_resident_deposition_rank

--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_hanoi_hadamard_order_site_rungs_algo/test_scry/test_calc_resident_deposition_rank.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_hanoi_hadamard_order_site_rungs_algo/test_scry/test_calc_resident_deposition_rank.py
@@ -1,4 +1,11 @@
+import typing
+
+import pytest
+
 from hsurf.hsurf import hanoi_hadamard_order_site_rungs_algo as algo
+from hsurf.site_selection_strategy.site_selection_algorithms.hanoi_hadamard_order_site_rungs_algo._impl import (
+    get_surface_rank_capacity,
+)
 
 
 def test_calc_resident_deposition_rank_unit():
@@ -212,3 +219,52 @@ def test_calc_resident_deposition_rank_unit():
         # 9,  # 2,  # 1,  # 21
         # fmt: on
     ]
+
+
+@pytest.mark.parametrize(
+    "surface_size",
+    [
+        2**x for x in range(2, 6)
+    ] + [
+        pytest.param(2**x, marks=pytest.mark.heavy)
+        for x in range(6, 11)
+    ],
+)
+@pytest.mark.parametrize(
+    "num_generations_bidder",
+    [
+        lambda surface_size: 2**14 // surface_size,
+        pytest.param(
+            lambda surface_size: 2**18 // max(1, surface_size >> 9),
+            marks=pytest.mark.heavy,
+        ),
+    ],
+)
+def test_calc_resident_deposition_rank_integration(
+    surface_size: int,
+    num_generations_bidder: typing.Callable,
+):
+    num_generations = min(
+        num_generations_bidder(surface_size),
+        get_surface_rank_capacity(surface_size) - 1,
+    )
+    surface_deposition_ranks = [0] * surface_size
+    for rank in range(num_generations):
+        target_site = algo.pick_deposition_site(rank, surface_size)
+        surface_deposition_ranks[target_site] = rank
+
+        for site, actual_deposition_rank in enumerate(
+            surface_deposition_ranks
+        ):
+            calculated_deposition_rank = algo.calc_resident_deposition_rank(
+                site,
+                surface_size,
+                rank + 1,
+            )
+            assert calculated_deposition_rank == actual_deposition_rank, {
+                "actual deposition rank": actual_deposition_rank,
+                "calculated deposition rank": calculated_deposition_rank,
+                "num depositions": rank + 1,
+                "rank": rank,
+                "site": site,
+            }
