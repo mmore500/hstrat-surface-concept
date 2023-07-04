116f388ce6495b55e3e718e395a4b96323a283c5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-07-03 20:30:02 -0700

    Use bitmagic for hanoi utils

--- pylib/hanoi/get_hanoi_value_index_offset.py
+++ pylib/hanoi/get_hanoi_value_index_offset.py
@@ -1,2 +1,2 @@
 def get_hanoi_value_index_offset(value: int) -> int:
-    return 2**value - 1
+    return (1 << value) - 1  # 2**value - 1
--- pylib/hanoi/get_incidence_count_of_hanoi_value_through_index.py
+++ pylib/hanoi/get_incidence_count_of_hanoi_value_through_index.py
@@ -1,3 +1,4 @@
+from ..fast_pow2_divide import fast_pow2_divide
 from .get_hanoi_value_index_cadence import get_hanoi_value_index_cadence
 from .get_hanoi_value_index_offset import get_hanoi_value_index_offset
 
@@ -11,4 +12,4 @@ def get_incidence_count_of_hanoi_value_through_index(
     """
     offset = get_hanoi_value_index_offset(value)
     cadence = get_hanoi_value_index_cadence(value)
-    return (n - offset + cadence) // cadence
+    return fast_pow2_divide(n - offset + cadence, cadence)
