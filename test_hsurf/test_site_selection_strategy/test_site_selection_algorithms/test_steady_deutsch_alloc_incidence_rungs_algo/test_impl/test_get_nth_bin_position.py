3eaa2a31b4e0fb2ca56cd7a5a38954abc3806b49
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-03 00:21:53 -0500

    Factor for use of get_num_bins helper

--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_steady_deutsch_alloc_incidence_rungs_algo/test_impl/test_get_nth_bin_position.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_steady_deutsch_alloc_incidence_rungs_algo/test_impl/test_get_nth_bin_position.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from hsurf.site_selection_strategy.site_selection_algorithms.steady_deutsch_alloc_incidence_rungs_algo._impl import (
+    get_num_bins,
     get_nth_bin_position,
     get_nth_bin_width,
 )
@@ -10,12 +11,13 @@ from hsurf.site_selection_strategy.site_selection_algorithms.steady_deutsch_allo
 @pytest.mark.parametrize("surface_size", [2**i for i in range(20)])
 def test_get_nth_bin_width(surface_size: int):
     bins = [
-        get_nth_bin_width(n, surface_size) for n in range(surface_size // 2)
+        get_nth_bin_width(n, surface_size)
+        for n in range(get_num_bins(surface_size))
     ]
     assert [
         0,
         *np.cumsum(bins),
     ] == [  # calculate expected positions from widths
         get_nth_bin_position(n, surface_size)
-        for n in range(surface_size // 2 + 1)
+        for n in range(get_num_bins(surface_size) + 1)
     ]
--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_steady_deutsch_alloc_incidence_rungs_algo/test_impl/test_get_nth_bin_width.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_steady_deutsch_alloc_incidence_rungs_algo/test_impl/test_get_nth_bin_width.py
@@ -5,13 +5,15 @@ import pytest
 
 from hsurf.site_selection_strategy.site_selection_algorithms.steady_deutsch_alloc_incidence_rungs_algo._impl import (
     get_nth_bin_width,
+    get_num_bins,
 )
 
 
 @pytest.mark.parametrize("surface_size", [2**i for i in range(20)])
 def test_get_nth_bin_width(surface_size: int):
     bins = [
-        get_nth_bin_width(n, surface_size) for n in range(surface_size // 2)
+        get_nth_bin_width(n, surface_size)
+        for n in range(get_num_bins(surface_size))
     ]
 
     if surface_size == 1:  # special case the trivial case
