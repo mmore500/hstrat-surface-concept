196ca997c9d1e219e8179a71bf5930ceced8999a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-03 16:05:05 -0500

    fixup! Draft iterative soln for get_bin_width_at_position

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_bin_width_at_position.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_bin_width_at_position.py
@@ -1,11 +1,6 @@
 import itertools as it
 
-import interval_search as inch
-import opytional as opyt
-
-from ._get_nth_bin_position import get_nth_bin_position
 from ._get_nth_bin_width import get_nth_bin_width
-from ._get_num_bins import get_num_bins
 
 
 def get_bin_width_at_position(position: int, surface_size: int) -> int:
