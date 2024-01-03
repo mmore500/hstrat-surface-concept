2ba6b761640541d299b6bedc5d76c47ce08b40c5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-03 14:59:48 -0500

    Impl closed form get_bin_number_of_position

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_bin_number_of_position.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_bin_number_of_position.py
@@ -1,17 +1,36 @@
-import interval_search as inch
-import opytional as opyt
-
+from ._get_bin_width_at_position import get_bin_width_at_position
 from ._get_nth_bin_position import get_nth_bin_position
-from ._get_num_bins import get_num_bins
+from ._get_nth_bin_width import get_nth_bin_width
 
 
 def get_bin_number_of_position(position: int, surface_size: int) -> int:
-    # TODO: closed-form solution or at least binary search
-    return opyt.or_value(
-        inch.binary_search(
-            lambda i: get_nth_bin_position(i + 1, surface_size) > position,
-            0,
-            get_num_bins(surface_size) - 2,
-        ),
-        get_num_bins(surface_size) - 1,
+    assert surface_size.bit_count() == 1  # assume power of two
+    assert position < surface_size - 1  # exclude special-cased position zero
+
+    bin_width = get_bin_width_at_position(position, surface_size)
+    first_bin_width = get_nth_bin_width(0, surface_size)
+    bin_segment_number = first_bin_width - bin_width
+    if bin_segment_number == 0:
+        return 0
+
+    bin_segment_first_bin_number = 1 << (first_bin_width - bin_width - 1)
+    assert (
+        get_nth_bin_width(bin_segment_first_bin_number, surface_size)
+        == bin_width
     )
+    assert bin_segment_first_bin_number  # segment 0 is special-cased
+    assert (
+        get_nth_bin_width(bin_segment_first_bin_number - 1, surface_size)
+        == bin_width + 1
+    )
+
+    bin_segment_first_bin_position = get_nth_bin_position(
+        bin_segment_first_bin_number, surface_size
+    )
+    assert bin_segment_first_bin_position <= position
+
+    bin_number_within_segment = (
+        position - bin_segment_first_bin_position
+    ) // bin_width
+
+    return bin_segment_first_bin_number + bin_number_within_segment
