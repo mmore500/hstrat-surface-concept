cb3a31234e4294abadcef1e76a7192f91cff8286
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-04 14:22:06 -0500

    Implement, test get_nth_segment_position

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/__init__.py
@@ -2,6 +2,7 @@ from ._get_bin_number_of_position import get_bin_number_of_position
 from ._get_bin_width_at_position import get_bin_width_at_position
 from ._get_nth_bin_position import get_nth_bin_position
 from ._get_nth_bin_width import get_nth_bin_width
+from ._get_nth_segment_position import get_nth_segment_position
 from ._get_num_bins import get_num_bins
 from ._get_num_segments import get_num_segments
 
@@ -10,6 +11,7 @@ __all__ = [
     "get_bin_width_at_position",
     "get_nth_bin_position",
     "get_nth_bin_width",
+    "get_nth_segment_position",
     "get_num_bins",
     "get_num_segments",
 ]
