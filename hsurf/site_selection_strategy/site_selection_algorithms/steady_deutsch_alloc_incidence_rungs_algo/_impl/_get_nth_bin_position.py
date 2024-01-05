f0908975f5730e3cb42b13a48c4ca4250c0fc310
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-04 20:07:11 -0500

    Refactor to consolidate nth bin pos magic

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_nth_bin_position.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_deutsch_alloc_incidence_rungs_algo/_impl/_get_nth_bin_position.py
@@ -1,4 +1,6 @@
 from .....pylib import bit_floor
+from ._get_nth_segment_bin_width import get_nth_segment_bin_width
+from ._get_nth_segment_position import get_nth_segment_position
 from ._get_num_bins import get_num_bins
 
 
@@ -7,15 +9,10 @@ def get_nth_bin_position(n: int, surface_size: int) -> int:
     # over does the nth bin start?
     # this is the sum of the widths of all bins before the nth bin
     assert 0 <= n < get_num_bins(surface_size)
-
-    if n == 0:
-        return 0
-
     assert surface_size.bit_count() == 1  # assume perfect power of 2
-    mbw = surface_size.bit_length() - 1  # max bin width, num trailing zeros
 
-    position = mbw  # special case the first one-bin segment
-    mbw -= 1  # new largest bin width is one less than the first
+    if n == 0:  # special-case the zeroth segment
+        return 0
     n -= 1  # we've handled one bin
 
     # get the next all-1s number less than or equal to n
@@ -23,26 +20,16 @@ def get_nth_bin_position(n: int, surface_size: int) -> int:
     completed_bins = bit_floor(n + 1) - 1
     assert completed_bins.bit_length() == completed_bins.bit_count()
     assert n // 2 <= completed_bins <= n
-    # assert lt == completed_bins
-    ncs = completed_bins.bit_length()  # num completed segments
+    completed_segments = 1 + completed_bins.bit_length()  # include 0th segment
 
-    # clever/tricky part...
-    # one-shot sum of all bin widths segments that are completely full...
-    # equivalent to iterative sum of bin segment sizes times bin width
-    # where bin width segment size doubles each iteration and bin segment size
-    # decreases by one each iteration
-
-    # https://www.wolframalpha.com/input?i=%5Csum_%7Bj%3D0%7D%5E%7Bb-1%7D+q+%5Ccdot+2%5Ej
-    position += (1 << ncs) * mbw - mbw
-    # equiv: cum += sum(mbw << j for j in range(ncs))
-
-    # https://www.wolframalpha.com/input?i=-2+%28-1+%2B+2%5En%29+%2B+2%5En+n&assumption=%22ClashPrefs%22+-%3E+%7B%22Math%22%7D
-    position -= ncs * (1 << ncs) - (1 << (ncs + 1)) + 2
-    # equiv: cum -=  sum(j << j for j in range(ncs))
+    position = get_nth_segment_position(completed_segments, surface_size)
 
     # ... and now handle the remaining partially-filled bin segment, if any
     num_unhandled_bins = n - completed_bins
-    unhandled_segment_bin_width = mbw - ncs
-    position += num_unhandled_bins * unhandled_segment_bin_width
+    if num_unhandled_bins:
+        unhandled_segment_bin_width = get_nth_segment_bin_width(
+            completed_segments, surface_size
+        )
+        position += num_unhandled_bins * unhandled_segment_bin_width
 
     return position
