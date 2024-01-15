750531548484a1b2f565f9a7c2e62ce1faad24ab
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-15 11:50:12 -0500

    Make plotter more robust to edge cases

--- pylib/site_selection_viz/_apply_pseudo_linear_yticks.py
+++ pylib/site_selection_viz/_apply_pseudo_linear_yticks.py
@@ -1,3 +1,5 @@
+import warnings
+
 from matplotlib import axes as mpl_axes
 
 from ..round_to_one_sigfig import round_to_one_sigfig
@@ -7,6 +9,9 @@ def apply_pseudo_linear_yticks(ax: mpl_axes.Axes) -> mpl_axes.Axes:
     # Replace categorical y-axis labels with (pseudo) linear axis
     maxval = max(int(label.get_text()) for label in ax.get_yticklabels())
     precision = round_to_one_sigfig(maxval // 10)
+    if precision == 0:
+        warnings.warn("Precision is 0, aborting pseudo-linear y-axis labeling")
+        return ax
     ax.set_yticklabels(
         [
             r"{:.1e}".format(
@@ -24,7 +29,7 @@ def apply_pseudo_linear_yticks(ax: mpl_axes.Axes) -> mpl_axes.Axes:
     # This will automatically remove duplicates because keys in a dictionary
     # must be unique.
     unique_ticks_and_labels = dict(reversed([*zip(labels, ticks)]))
-    assert len(unique_ticks_and_labels) < len(labels)
+    assert len(unique_ticks_and_labels) <= len(labels)
 
     # Set the ticks and labels
     ax.set_yticks([*reversed([*unique_ticks_and_labels.values()])])
