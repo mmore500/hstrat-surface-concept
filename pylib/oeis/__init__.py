16cff291b5687ccb5d6e6bc1efd361c388d008c5
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-15 00:38:12 -0500

    Impl, test oeis a048881 impl

--- pylib/oeis/__init__.py
+++ pylib/oeis/__init__.py
@@ -3,6 +3,7 @@ from .get_a000295_value_at_index import get_a000295_value_at_index
 from .get_a025480_value_at_index import get_a025480_value_at_index
 from .get_a030109_index_of_value import get_a030109_index_of_value
 from .get_a030109_value_at_index import get_a030109_value_at_index
+from .get_a048881_value_at_index import get_a048881_value_at_index
 from .get_a059893_index_of_value import get_a059893_index_of_value
 from .get_a059893_value_at_index import get_a059893_value_at_index
 from .get_a083058_index_of_value import get_a083058_index_of_value
@@ -16,6 +17,7 @@ __all__ = [
     "get_a025480_value_at_index",
     "get_a030109_index_of_value",
     "get_a030109_value_at_index",
+    "get_a048881_value_at_index",
     "get_a059893_index_of_value",
     "get_a059893_value_at_index",
     "get_a083058_index_of_value",
