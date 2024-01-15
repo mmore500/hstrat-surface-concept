008bb03971922aefa3474beda34e56bc5eb5a526
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-15 02:43:23 -0500

    Impl, test incidence-runing for eulerian tilted

--- hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/_impl/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/_impl/__init__.py
@@ -1,5 +1,6 @@
 from ._get_global_epoch import get_global_epoch
 from ._get_global_num_reservations import get_global_num_reservations
+from ._get_hanoi_num_reservations import get_hanoi_num_reservations
 from ._get_reservation_position_logical import get_reservation_position_logical
 from ._get_reservation_position_physical import (
     get_reservation_position_physical,
@@ -9,6 +10,7 @@ from ._get_reservation_position_physical import (
 __all__ = [
     "get_global_epoch",
     "get_global_num_reservations",
+    "get_hanoi_num_reservations",
     "get_reservation_position_logical",
     "get_reservation_position_physical",
 ]
