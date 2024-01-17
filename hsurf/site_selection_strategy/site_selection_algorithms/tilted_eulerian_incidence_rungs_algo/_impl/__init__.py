038e49bddbca27336f9698ce2d82b842f63fa19e
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-16 21:08:51 -0500

    Impl, test euler get site genesis reservation index

--- hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/_impl/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/_impl/__init__.py
@@ -5,6 +5,9 @@ from ._get_reservation_position_logical import get_reservation_position_logical
 from ._get_reservation_position_physical import (
     get_reservation_position_physical,
 )
+from ._get_site_genesis_reservation_index_physical import (
+    get_site_genesis_reservation_index_physical,
+)
 
 __all__ = [
     "get_global_epoch",
@@ -12,4 +15,5 @@ __all__ = [
     "get_hanoi_num_reservations",
     "get_reservation_position_logical",
     "get_reservation_position_physical",
+    "get_site_genesis_reservation_index_physical",
 ]
