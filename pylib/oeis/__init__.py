ec6e8081655a6a0f1410305e5056498bf322101a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-16 20:22:37 -0500

    Impl, test oeis a005187 helper

--- pylib/oeis/__init__.py
+++ pylib/oeis/__init__.py
@@ -1,5 +1,6 @@
 from .get_a000295_index_of_value import get_a000295_index_of_value
 from .get_a000295_value_at_index import get_a000295_value_at_index
+from .get_a005187_value_at_index import get_a005187_value_at_index
 from .get_a025480_value_at_index import get_a025480_value_at_index
 from .get_a030109_index_of_value import get_a030109_index_of_value
 from .get_a030109_value_at_index import get_a030109_value_at_index
@@ -14,6 +15,7 @@ from .get_a341916_value_at_index import get_a341916_value_at_index
 __all__ = [
     "get_a000295_index_of_value",
     "get_a000295_value_at_index",
+    "get_a005187_value_at_index",
     "get_a025480_value_at_index",
     "get_a030109_index_of_value",
     "get_a030109_value_at_index",
