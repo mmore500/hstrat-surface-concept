4c037b330af8b8b0877472d90361e70c0be1ec8d
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-18 12:11:05 -0500

    Increase sequence test size

--- pylib/test/test_oeis/test_get_a005187_index_of_value.py
+++ pylib/test/test_oeis/test_get_a005187_index_of_value.py
@@ -2,7 +2,7 @@ from pylib import oeis
 
 
 def test_get_a005187_index_of_value():
-    for i in range(2000):
+    for i in range(1000000):
         n = oeis.get_a005187_index_of_value(i)
         lb = oeis.get_a005187_value_at_index(n)
         ub = oeis.get_a005187_value_at_index(n + 1)
