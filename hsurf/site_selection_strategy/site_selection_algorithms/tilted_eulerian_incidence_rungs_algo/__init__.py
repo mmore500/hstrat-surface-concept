866f523f1cf1337a73f42e778708d678f7005bae
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-17 19:46:54 -0500

    Add calc_resident_deposition_rank to __all__

--- hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_incidence_rungs_algo/__init__.py
@@ -2,5 +2,6 @@ from ._enact._pick_deposition_site import pick_deposition_site
 from ._scry._calc_resident_deposition_rank import calc_resident_deposition_rank
 
 __all__ = [
+    "calc_resident_deposition_rank",
     "pick_deposition_site",
 ]
