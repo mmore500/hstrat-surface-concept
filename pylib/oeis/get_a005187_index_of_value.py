5912e5c43b92784f6467af636261eb0b06bfeb4b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-18 10:32:58 -0500

    Set tigher bounds on a005187 search

--- pylib/oeis/get_a005187_index_of_value.py
+++ pylib/oeis/get_a005187_index_of_value.py
@@ -6,6 +6,8 @@ from .get_a005187_value_at_index import get_a005187_value_at_index
 # https://oeis.org/A005187
 def get_a005187_index_of_value(n: int) -> int:
     """Get the index of the largest value of A005187 less than or equal to n."""
-    return inch.doubling_search(
+    return inch.binary_search(
         lambda x: get_a005187_value_at_index(x + 1) > n,
+        n >> 1,
+        (n >> 1) + n.bit_length(),
     )
