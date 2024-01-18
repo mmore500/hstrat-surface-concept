0a05431b9a60b0b818e229893d1bd3b8c1cc132f
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-17 21:20:36 -0500

    Impl, test stickified deposition rank lookup

---
+++ hsurf/site_selection_strategy/site_selection_algorithms/tilted_eulerian_stickified_algo/_iter_resident_deposition_ranks.py
@@ -0,0 +1,21 @@
+import typing
+
+from ._calc_resident_deposition_rank import calc_resident_deposition_rank
+
+
+def iter_resident_deposition_ranks(
+    surface_size: int, num_depositions: int
+) -> typing.Iterable[int]:
+    """When `num_depositions` deposition cycles have elapsed, what is the
+    deposition rank of the stratum resident at each site?
+
+    Yields deposition ranks in order of site index, from site 0 up to site
+    `surface_size`. Returns 0 if the resident stratum traces back to original
+    randomization of the surface prior to any algorithm-determined stratum
+    depositions.
+
+    Somewhat (conceptually) inverse to `pick_deposition_site`.
+    """
+    # naive implementation for now
+    for site in range(surface_size):
+        yield calc_resident_deposition_rank(site, surface_size, num_depositions)
