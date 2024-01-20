f96ce19f6e50f313becffc1a3514ee8c7f61a2aa
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-20 00:01:52 -0500

    Add tests for interop CalcRankAtColumnIndex

--- test_hsurf/test_interop/test_make_stratum_retention_policy_from_site_selection_algo.py
+++ test_hsurf/test_interop/test_make_stratum_retention_policy_from_site_selection_algo.py
@@ -55,3 +55,35 @@ def test_CalcNumStrataRetainedExact(
         assert policy.CalcNumStrataRetainedExact(rank) == min(
             rank, surface_size
         )
+
+
+@pytest.mark.parametrize(
+    "interop_algo",
+    [
+        hsurf.stratum_retention_interop_steady_algo,
+        hsurf.stratum_retention_interop_tilted_algo,
+        hsurf.stratum_retention_interop_tilted_sticky_algo,
+    ],
+)
+@pytest.mark.parametrize(
+    "surface_size",
+    [8, 16],
+)
+def test_CalcRankAtColumnIndex(
+    interop_algo: types.ModuleType, surface_size: int
+):
+    policy = interop_algo.Policy(surface_size)
+
+    assert policy.CalcNumStrataRetainedExact(0) == 0
+    for rank in range(1, 100):
+        column_ranks = [
+            policy.CalcRankAtColumnIndex(index, rank)
+            for index in range(policy.CalcNumStrataRetainedExact(rank))
+        ]
+        assert column_ranks == sorted(
+            set(  # have to deduplicate rank 0 entries
+                interop_algo._site_selection_algo.iter_resident_deposition_ranks(
+                    surface_size, rank
+                ),
+            ),
+        )
