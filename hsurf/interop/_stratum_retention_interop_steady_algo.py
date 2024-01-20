22ce030a04b558323b6d7b222a28ad08174a4211
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-20 02:37:28 -0500

    Fix isort lint

--- hsurf/interop/_stratum_retention_interop_steady_algo.py
+++ hsurf/interop/_stratum_retention_interop_steady_algo.py
@@ -1,8 +1,8 @@
-from ._make_stratum_retention_policy_from_site_selection_algo import (
-    make_stratum_retention_policy_from_site_selection_algo,
-)
 from ..site_selection_strategy.site_selection_algorithms import (
     steady_algo as _site_selection_algo,
+)
+from ._make_stratum_retention_policy_from_site_selection_algo import (
+    make_stratum_retention_policy_from_site_selection_algo,
 )
 
 Policy = make_stratum_retention_policy_from_site_selection_algo(
