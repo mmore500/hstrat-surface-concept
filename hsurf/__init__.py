68dfdcdc9d3eb1b7fabcf06eb2bb6ccbc755970a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-01-19 20:01:56 -0500

    fixup! Add packaging infrastructure

--- hsurf/__init__.py
+++ hsurf/__init__.py
@@ -1,3 +1,10 @@
+"""Top-level package for hsurf."""
+
+__author__ = "Matthew Andres Moreno"
+__email__ = "m.more500@gmail.com"
+__version__ = "0.0.0"
+
+
 from . import genome_instrumentation, site_selection_strategy
 
 __all__ = [
