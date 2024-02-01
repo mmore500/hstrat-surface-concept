20cc462415ab73cc137baaa8ab27a3a2e93f2be9
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-02-01 02:33:08 -0500

    bump version to v0.2.0

--- hsurf/__init__.py
+++ hsurf/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Matthew Andres Moreno"
 __email__ = "m.more500@gmail.com"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 from . import genome_instrumentation, interop, site_selection_strategy
