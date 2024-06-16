462176279034be5dfc57e0c5ecbe11cd8c75d74b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-16 19:08:21 -0400

    Fixup calc_next_invasion_rank

--- hsurf/site_selection_strategy/site_selection_algorithms/stretched_algo/_impl/_calc_next_invasion_rank.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/stretched_algo/_impl/_calc_next_invasion_rank.py
@@ -1,5 +1,5 @@
 from .....pylib import hanoi, oeis
-from ...tilted_algo._impl import get_global_epoch
+from ...tilted_algo._impl import get_global_epoch, get_hanoi_num_reservations
 
 
 def calc_next_invasion_rank(rank: int, surface_size: int) -> int:
@@ -8,6 +8,13 @@ def calc_next_invasion_rank(rank: int, surface_size: int) -> int:
     epoch = get_global_epoch(rank, surface_size)
     assert epoch
     min_hanoi_invader = oeis.get_a062289_value_at_index(epoch - 1)
+    assert min_hanoi_invader
+    while get_hanoi_num_reservations(
+        rank, surface_size, min_hanoi_invader
+    ) <= hanoi.get_incidence_count_of_hanoi_value_through_index(
+        min_hanoi_invader, rank
+    ):
+        min_hanoi_invader += 1
     invasion_rank = hanoi.get_index_of_next_hanoi_value_geq_to(
         min_hanoi_invader, rank
     )
--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_impl/test_calc_next_invasion_rank.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_impl/test_calc_next_invasion_rank.py
@@ -13,12 +13,18 @@ from pylib import hanoi
 
 @pytest.mark.parametrize("surface_size", [8, 16, 32, 64, 128, 256, 512, 1024])
 def test_calc_next_invasion_rank(surface_size: int):
-    for rank in range(surface_size, 5000):
+    for rank in range(surface_size, min(1000, 2**surface_size - 1)):
         expected = next(
             ansatz_rank
-            for ansatz_rank in it.count(rank)
+            for ansatz_rank in it.count(rank + 1)
             if hanoi.get_hanoi_value_incidence_at_index(ansatz_rank)
             < get_hanoi_num_reservations(ansatz_rank, surface_size)
         )
-        actual = calc_next_invasion_rank(rank, surface_size)
-        assert expected == actual
+        assert hanoi.get_hanoi_value_at_index(expected) >= 2, (
+            expected,
+            get_hanoi_num_reservations(expected, surface_size),
+            hanoi.get_hanoi_value_incidence_at_index(expected),
+        )
+        assert expected > rank
+        calculated = calc_next_invasion_rank(rank, surface_size)
+        assert calculated == expected
