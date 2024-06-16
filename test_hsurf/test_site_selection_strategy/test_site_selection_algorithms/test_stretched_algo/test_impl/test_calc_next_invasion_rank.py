02796bd152e08c7c314ed580194dd902d97f3adb
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-16 18:17:28 -0400

    Add failing test for calc_next_invasion_rank

---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_impl/test_calc_next_invasion_rank.py
@@ -0,0 +1,24 @@
+import itertools as it
+
+import pytest
+
+from hsurf.site_selection_strategy.site_selection_algorithms.stretched_algo._impl import (
+    calc_next_invasion_rank,
+)
+from hsurf.site_selection_strategy.site_selection_algorithms.tilted_algo._impl import (
+    get_hanoi_num_reservations,
+)
+from pylib import hanoi
+
+
+@pytest.mark.parametrize("surface_size", [8, 16, 32, 64, 128, 256, 512, 1024])
+def test_calc_next_invasion_rank(surface_size: int):
+    for rank in range(surface_size, 5000):
+        expected = next(
+            ansatz_rank
+            for ansatz_rank in it.count(rank)
+            if hanoi.get_hanoi_value_incidence_at_index(ansatz_rank)
+            < get_hanoi_num_reservations(ansatz_rank, surface_size)
+        )
+        actual = calc_next_invasion_rank(rank, surface_size)
+        assert expected == actual
