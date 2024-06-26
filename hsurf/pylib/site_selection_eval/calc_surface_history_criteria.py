3f8aaec4efbd17964f3a2f02f88a669eadf3e7be
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-26 19:45:43 -0400

    Draft gap ratio bounds, set for stretched/tilted

--- hsurf/pylib/site_selection_eval/calc_surface_history_criteria.py
+++ hsurf/pylib/site_selection_eval/calc_surface_history_criteria.py
@@ -7,9 +7,11 @@ import pandas as pd
 def calc_surface_history_criteria(
     surface_history_df: pd.DataFrame,
     progress_wrap: typing.Callable = lambda x: x,
+    enforce_tilted_bound: bool = False,
+    enforce_stretched_bound: bool = False,
 ) -> pd.DataFrame:
 
-    surface_size = surface_history_df["site"].max() + 1
+    surface_size = int(surface_history_df["site"].max() + 1)
 
     records = []
     for rank, group_df in progress_wrap(
@@ -100,8 +102,26 @@ def calc_surface_history_criteria(
             },
         )
 
-        gap_ratio_upper_bound = np.nan  # TODO
-        # ... add consistency assertions
+        if rank >= surface_size:
+            epoch = rank.bit_length() - surface_size.bit_length() + 1
+            gap_ratio_upper_bound = min(
+                2 * (epoch + surface_size.bit_length()) / surface_size,
+                4 * epoch / surface_size,
+            )
+        else:
+            gap_ratio_upper_bound = 0
+
+        if enforce_stretched_bound:
+            assert (
+                gap_ratio_upper_bound >= gap_stretched_ratios.max()
+                or np.isclose(gap_ratio_upper_bound, gap_stretched_ratios.max())
+            )
+        if enforce_tilted_bound:
+            assert (
+                gap_ratio_upper_bound >= gap_tilted_ratios.max()
+                or np.isclose(gap_ratio_upper_bound, gap_tilted_ratios.max())
+            )
+
         records.append(
             {
                 "rank": rank,
