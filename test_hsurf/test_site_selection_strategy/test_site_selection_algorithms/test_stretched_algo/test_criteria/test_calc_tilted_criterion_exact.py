240f80775360bfff45ca9c479a605ae1a9a5f137
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 19:50:51 -0400

    Impl, test criteria calculations for stretched algo

---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_criteria/test_calc_tilted_criterion_exact.py
@@ -0,0 +1,23 @@
+import pytest
+
+from hsurf.hsurf import steady_algo as algo
+
+
+@pytest.mark.parametrize(
+    "surface_size, num_depositions, expected_criterion_value",
+    [
+        (8, 0, 0),
+        (8, 1, 0),
+        (8, 8, 0),
+        (8, 9, 0.25),
+    ],
+)
+def test_calc_tilted_criterion_exact(
+    surface_size: int,
+    num_depositions: int,
+    expected_criterion_value: int,
+):
+    assert (
+        algo.calc_tilted_criterion_exact(surface_size, num_depositions)
+        == expected_criterion_value
+    )
