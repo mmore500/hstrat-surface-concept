e7d7f9d1f8ea408cca301048fc5f0d3d19b57253
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 14:37:33 -0400

    Fix copy/paste mistake testing wrong algos

--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_scry/test_iter_resident_deposition_ranks.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_algo/test_scry/test_iter_resident_deposition_ranks.py
@@ -3,7 +3,7 @@ import itertools as it
 import numpy as np
 import pytest
 
-from hsurf.hsurf import tilted_algo as algo
+from hsurf.hsurf import stretched_algo as algo
 
 
 @pytest.mark.parametrize("surface_size", [2**x for x in range(1, 10)])
@@ -19,6 +19,8 @@ from hsurf.hsurf import tilted_algo as algo
 def test_iter_resident_deposition_ranks(
     surface_size: int, num_depositions: int
 ) -> int:
+    if num_depositions >= 2 ** (surface_size.bit_length() - 1) - 1:
+        return
     expected = (
         algo.calc_resident_deposition_rank(site, surface_size, num_depositions)
         for site in range(surface_size)
