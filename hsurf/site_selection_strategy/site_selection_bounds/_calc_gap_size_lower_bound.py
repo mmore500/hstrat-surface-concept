6b8bad2c4676b7c0a807b76283c8c327758eb2f6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 06:07:20 -0400

    Add docstring

--- hsurf/site_selection_strategy/site_selection_bounds/_calc_gap_size_lower_bound.py
+++ hsurf/site_selection_strategy/site_selection_bounds/_calc_gap_size_lower_bound.py
@@ -1,4 +1,18 @@
 def calc_gap_size_lower_bound(rank: int, surface_size: int) -> int:
+    """Calculate the lower bound of gap size at time step `rank`.
+
+    Parameters
+    ----------
+    rank : int
+        The current time step.
+    surface_size : int
+        The maximum number of items that can be retained.
+
+    Returns
+    -------
+    int
+        The lowest-possible largest set of consecutive dropped elements.
+    """
     naive = rank // surface_size
     res = (max(rank + 1 - surface_size, 0) + surface_size) // (surface_size + 1)
     # res = (max(rank + 1 - surface_size, 0) + surface_size) // (surface_size + 1)
