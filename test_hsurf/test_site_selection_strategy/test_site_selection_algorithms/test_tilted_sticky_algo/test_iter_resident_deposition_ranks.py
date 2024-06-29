07586783b1f7e9002c4a15c8f7fe44d3e061495b
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 13:54:20 -0400

    Fix misnamed param in iter ranks tests

--- test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_tilted_sticky_algo/test_iter_resident_deposition_ranks.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_tilted_sticky_algo/test_iter_resident_deposition_ranks.py
@@ -8,7 +8,7 @@ from hsurf.hsurf import tilted_sticky_algo as algo
 
 @pytest.mark.parametrize("surface_size", [2**x for x in range(1, 12)])
 @pytest.mark.parametrize(
-    "rank",
+    "num_depositions",
     [
         *range(128),
         *map(int, np.linspace(0, 2**62, 100, dtype=int)),
@@ -16,13 +16,15 @@ from hsurf.hsurf import tilted_sticky_algo as algo
         *map(int, np.random.RandomState(seed=1).randint(0, 2**62, 100)),
     ],
 )
-def test_iter_resident_deposition_ranks(surface_size: int, rank: int) -> int:
-    if rank > 2 ** (surface_size.bit_length() - 1) - 1:
+def test_iter_resident_deposition_ranks(
+    surface_size: int, num_depositions: int
+) -> int:
+    if num_depositions > 2 ** (surface_size.bit_length() - 1) - 1:
         return
 
     expected = (
-        algo.calc_resident_deposition_rank(site, surface_size, rank)
+        algo.calc_resident_deposition_rank(site, surface_size, num_depositions)
         for site in range(surface_size)
     )
-    actual = algo.iter_resident_deposition_ranks(surface_size, rank)
+    actual = algo.iter_resident_deposition_ranks(surface_size, num_depositions)
     assert all(it.starmap(int.__eq__, zip(actual, expected, strict=True)))
