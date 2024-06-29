fe91ad1f135b08f0c59cb8ef04eb51d6dfe343ff
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 06:27:59 -0400

    Impl, test calc {tilted,stretched} lower bounds

--- hsurf/site_selection_strategy/site_selection_bounds/__init__.py
+++ hsurf/site_selection_strategy/site_selection_bounds/__init__.py
@@ -1,7 +1,11 @@
 from ._calc_gap_size_lower_bound import calc_gap_size_lower_bound
+from ._calc_stretched_ratio_lower_bound import calc_stretched_ratio_lower_bound
+from ._calc_tilted_ratio_lower_bound import calc_tilted_ratio_lower_bound
 from ._optimize_criterion import optimize_criterion
 
 __all__ = [
     "calc_gap_size_lower_bound",
+    "calc_stretched_ratio_lower_bound",
+    "calc_tilted_ratio_lower_bound",
     "optimize_criterion",
 ]
---
+++ hsurf/site_selection_strategy/site_selection_bounds/_calc_stretched_ratio_lower_bound.py
@@ -0,0 +1,28 @@
+from ._impl import calc_gap_ratio_lower_bound
+
+
+def calc_stretched_ratio_lower_bound(
+    rank: int,
+    surface_size: int,
+) -> float:
+    """Calculate the best-possible minimization of the stretched criterion at
+    time step `rank`.
+
+    Parameters
+    ----------
+    rank : int
+        The current time step.
+    surface_size : int
+        The maximum number of items that can be retained.
+
+    Returns
+    -------
+    int
+        The best-possible minimization of the stretched criterion at
+        time step `rank`.
+
+    See Also
+    --------
+    calc_gap_ratio_lower_bound : Implementation, shared with tilted criterion.
+    """
+    return calc_gap_ratio_lower_bound(rank, surface_size)
