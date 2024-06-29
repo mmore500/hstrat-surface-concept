8c96bebb85277621aab706208c232e9f38412eb3
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-28 21:10:41 -0400

    Make gap size lower bound more conservtive
    Take into account occupied ranks

--- hsurf/site_selection_strategy/site_selection_bounds/_calc_gap_size_lower_bound.py
+++ hsurf/site_selection_strategy/site_selection_bounds/_calc_gap_size_lower_bound.py
@@ -1,2 +1,8 @@
 def calc_gap_size_lower_bound(rank: int, surface_size: int) -> int:
-    return rank // surface_size
+    naive = rank // surface_size
+    res = (max(rank + 1 - surface_size, 0) + surface_size) // (surface_size + 1)
+    # res = (max(rank + 1 - surface_size, 0) + surface_size) // (surface_size + 1)
+    # aka
+    res = (rank + 1) // (surface_size + 1)
+    assert res <= naive
+    return res
--- test_hsurf/test_site_selection_strategy/test_site_selection_bounds/test_calc_gap_size_lower_bound.py
+++ test_hsurf/test_site_selection_strategy/test_site_selection_bounds/test_calc_gap_size_lower_bound.py
@@ -11,10 +11,10 @@ from hsurf.site_selection_strategy.site_selection_bounds import (
         (0, 10, 0),
         (9, 10, 0),
         (10, 10, 1),
-        (10, 1, 10),
-        (25, 5, 5),
+        (10, 1, 5),
+        (25, 5, 4),
         (24, 5, 4),
-        (100, 20, 5),
+        (100, 20, 4),
     ],
 )
 def test_calc_gap_size_lower_bound(rank, surface_size, expected):
