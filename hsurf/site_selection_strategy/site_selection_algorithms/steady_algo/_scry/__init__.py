8a576c5022b6a445ece8650bef58f6596465c7d4
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 17:19:17 -0400

    fixup! Impl, test iter_retained_deposition_ranks for algos

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_algo/_scry/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_algo/_scry/__init__.py
@@ -1,7 +1,9 @@
 from ._calc_resident_deposition_rank import calc_resident_deposition_rank
 from ._iter_resident_deposition_ranks import iter_resident_deposition_ranks
+from ._iter_retained_deposition_ranks import iter_retained_deposition_ranks
 
 __all__ = [
     "calc_resident_deposition_rank",
     "iter_resident_deposition_ranks",
+    "iter_retained_deposition_ranks",
 ]
