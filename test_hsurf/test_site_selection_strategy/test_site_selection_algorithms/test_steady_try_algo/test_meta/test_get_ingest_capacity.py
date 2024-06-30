1a2bc19a88f511ec02b3cc389233a963fda18b1a
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-30 10:58:00 -0400

    Impl, test capacity test for steady try algo

---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_steady_try_algo/test_meta/test_get_ingest_capacity.py
@@ -0,0 +1,22 @@
+import opytional as opyt
+import pytest
+
+from hsurf.hsurf import steady_try_algo as algo
+
+
+@pytest.mark.parametrize("surface_size", range(70))
+def test_get_ingest_capacity(surface_size: int):
+
+    res = algo.get_ingest_capacity(surface_size)
+
+    if res != 0:
+        algo.pick_ingest_site(0, surface_size)
+        algo.pick_ingest_site(opyt.or_value(res, 2**100), surface_size)
+        algo.calc_resident_ingest_rank(0, surface_size, 0)
+        algo.calc_resident_ingest_rank(
+            0, surface_size, opyt.or_value(res, 2**100)
+        )
+
+    if res is not None:
+        with pytest.raises(Exception):
+            algo.pick_ingest_site(res + 1, surface_size)
