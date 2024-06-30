185308ce40dc03fcd89388140efc4d921b777232
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-30 09:39:02 -0400

    Redraft import syntax for consistency

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_algo/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_algo/__init__.py
@@ -23,33 +23,23 @@ pass on sites that haven't been filled yet once the end of the waterfall is
 reached. That bookkeeping is not implemented in this version.
 """
 
-from ._criteria._calc_steady_criterion_exact import calc_steady_criterion_exact
-from ._criteria._calc_steady_criterion_lower_bound import (
+from ._criteria import (
+    calc_steady_criterion_exact,
     calc_steady_criterion_lower_bound,
-)
-from ._criteria._calc_steady_criterion_upper_bound import (
     calc_steady_criterion_upper_bound,
-)
-from ._criteria._calc_stretched_criterion_exact import (
     calc_stretched_criterion_exact,
-)
-from ._criteria._calc_stretched_criterion_lower_bound import (
     calc_stretched_criterion_lower_bound,
-)
-from ._criteria._calc_stretched_criterion_upper_bound import (
     calc_stretched_criterion_upper_bound,
-)
-from ._criteria._calc_tilted_criterion_exact import calc_tilted_criterion_exact
-from ._criteria._calc_tilted_criterion_lower_bound import (
+    calc_tilted_criterion_exact,
     calc_tilted_criterion_lower_bound,
-)
-from ._criteria._calc_tilted_criterion_upper_bound import (
     calc_tilted_criterion_upper_bound,
 )
 from ._enact._pick_ingest_site import pick_ingest_site
-from ._scry._calc_resident_ingest_rank import calc_resident_ingest_rank
-from ._scry._iter_resident_ingest_ranks import iter_resident_ingest_ranks
-from ._scry._iter_retained_ingest_ranks import iter_retained_ingest_ranks
+from ._scry import (
+    calc_resident_ingest_rank,
+    iter_resident_ingest_ranks,
+    iter_retained_ingest_ranks,
+)
 
 __all__ = [
     "calc_resident_ingest_rank",
--- hsurf/site_selection_strategy/site_selection_algorithms/steady_try_algo/__init__.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_try_algo/__init__.py
@@ -17,9 +17,11 @@ from ._criteria import (
     calc_tilted_criterion_upper_bound,
 )
 from ._enact._pick_ingest_site import pick_ingest_site
-from ._scry._calc_resident_ingest_rank import calc_resident_ingest_rank
-from ._scry._iter_resident_ingest_ranks import iter_resident_ingest_ranks
-from ._scry._iter_retained_ingest_ranks import iter_retained_ingest_ranks
+from ._scry import (
+    calc_resident_ingest_rank,
+    iter_resident_ingest_ranks,
+    iter_retained_ingest_ranks,
+)
 
 __all__ = [
     "calc_resident_ingest_rank",
