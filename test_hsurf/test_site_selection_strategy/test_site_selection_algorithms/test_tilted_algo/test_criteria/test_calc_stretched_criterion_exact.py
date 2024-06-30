c0dbe23bc5679952516f990db28040a4919f9794
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 20:27:11 -0400

    Add criteria for tilted algo

---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_tilted_algo/test_criteria/test_calc_stretched_criterion_exact.py
@@ -0,0 +1,26 @@
+import pytest
+
+from hsurf.hsurf import tilted_algo as algo
+
+
+@pytest.mark.parametrize(
+    "surface_size, num_depositions, expected_criterion_value",
+    [
+        (8, 0, 0),
+        (8, 1, 0),
+        (8, 8, 0),
+        (8, 9, 1.0),
+        (8, 10, 2.0),
+    ],
+)
+def test_calc_stretched_criterion_exact(
+    surface_size: int,
+    num_depositions: int,
+    expected_criterion_value: int,
+):
+    if num_depositions >= surface_size**2:
+        return
+    assert (
+        algo.calc_stretched_criterion_exact(surface_size, num_depositions)
+        == expected_criterion_value
+    )
---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_tilted_algo/test_criteria/test_calc_tilted_criterion_lower_bound.py
@@ -0,0 +1,27 @@
+import pytest
+
+from hsurf.hsurf import tilted_algo as algo
+from hsurf.site_selection_strategy.site_selection_bounds import (
+    calc_tilted_ratio_lower_bound as ideal_lb,
+)
+
+
+@pytest.mark.parametrize(
+    "surface_size",
+    [2**i for i in range(1, 10)],
+)
+@pytest.mark.parametrize(
+    "num_depositions",
+    [0, 1, 63, 64, 65, 80, 100, 1000, 1023, 1024, 1025],
+)
+def test_calc_tilted_criterion_lower_bound(
+    surface_size: int,
+    num_depositions: int,
+):
+    if num_depositions >= surface_size**2:
+        return
+    assert (
+        ideal_lb(surface_size, num_depositions)
+        <= algo.calc_tilted_criterion_lower_bound(surface_size, num_depositions)
+        <= algo.calc_tilted_criterion_exact(surface_size, num_depositions)
+    )
