d4c316216ebad89f35de8e335f34f15aa0275ca6
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-06-29 20:06:30 -0400

    Impl criteria for stretched try algo

---
+++ test_hsurf/test_site_selection_strategy/test_site_selection_algorithms/test_stretched_try_algo/test_criteria/test_calc_steady_criterion_upper_bound.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pytest
+
+from hsurf.hsurf import stretched_try_algo as algo
+
+
+@pytest.mark.parametrize(
+    "surface_size",
+    [2**i for i in range(1, 10)],
+)
+@pytest.mark.parametrize(
+    "num_depositions",
+    [0, 1, 63, 64, 65, 80, 100, 1000, 1023, 1024, 1025],
+)
+def test_calc_steady_criterion_lower_bound(
+    surface_size: int,
+    num_depositions: int,
+):
+    if num_depositions >= surface_size**2:
+        return
+    assert (
+        algo.calc_steady_criterion_exact(surface_size, num_depositions)
+        <= algo.calc_steady_criterion_upper_bound(surface_size, num_depositions)
+        <= num_depositions
+    )
