8a1cd7529dbcda2e0aa535c11ad933f113f69daf
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-07-01 10:31:16 -0400

    Delete dead code

--- hsurf/pylib/test/test_oeis/test_get_a000295_index_of_value.py
+++ hsurf/pylib/test/test_oeis/test_get_a000295_index_of_value.py
@@ -25,9 +25,6 @@ def test_get_a000295_value_at_index():
 
 for i in range(400):
     n = pylib.oeis.get_a000295_index_of_value(i)
-    lb, ub = pylib.oeis.get_a000295_value_at_index(
-        n
-    ), pylib.oeis.get_a000295_value_at_index(n + 1)
     assert (
         pylib.oeis.get_a000295_value_at_index(n)
         <= i
