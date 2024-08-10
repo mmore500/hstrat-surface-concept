a739db1f8d26a5073c88aad254606282254e49fd
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-08-09 22:12:07 -0400

    Document and beautify steady pseudocode

--- implemented_pseudocode/steady_site_selection.py
+++ implemented_pseudocode/steady_site_selection.py
@@ -2,34 +2,50 @@ import typing
 
 
 def ctz(x: int) -> int:
+    """Count trailing zeros."""
     assert x > 0
     return (x & -x).bit_length() - 1
 
 
 def bit_floor(x: int) -> int:
+    """Return the largest power of two less than or equal to x."""
     assert x > 0
     return 1 << (x.bit_length() - 1)
 
 
 def steady_site_selection(S: int, T: int) -> typing.Optional[int]:
-    s = S.bit_length() - 1
-    t = (T + 1).bit_length() - s
-    h = ctz(T + 1)
-    if h < t:
-        return None
+    """Site selection algorithm for steady curation.
 
-    i = T >> (h + 1)  # hanoi value incidence
-    if i == 0:
+    Parameters
+    ----------
+    S : int
+        Buffer size. Must be a power of two.
+    T : int
+        Current logical time.
+
+    Returns
+    -------
+    typing.Optional[int]
+        Selected site, if any.
+    """
+    s = S.bit_length() - 1
+    t = (T + 1).bit_length() - s  # Current epoch (or negative)
+    h = ctz(T + 1)  # Current hanoi value
+    if h < t:  # If not a top n(T) hanoi value...
+        return None  # ...discard without storing
+
+    i = T >> (h + 1)  # Hanoi value incidence
+    if i == 0:  # Special case the 0th bunch
         k = 0  # Bunch position
         o = 0  # Within-bunch offset
-        w = s  # segment width
+        w = s  # Segment width
     else:
-        j = (1 << (i.bit_length() - 1)) - 1  # complete-bunch segments
-        b = j.bit_length()  # bunch index minus one
-        k = (1 << b) * (s - b + 1) - 1  # bunch position
-        w = h - t + 1  # segment width
+        j = bit_floor(i) - 1  # Num full-bunch segments
+        b = j.bit_length()  # Num full bunches
+        k = (1 << b) * (s - b + 1) - 1  # Bunch position
+        w = h - t + 1  # Segment width
         assert w > 0
-        o = w * (i - j - 1)  # within-bunch offset
+        o = w * (i - j - 1)  # Within-bunch offset
 
-    p = h % w  # within-segment offset
-    return k + o + p
+    p = h % w  # Within-segment offset
+    return k + o + p  # Calculate placement site
