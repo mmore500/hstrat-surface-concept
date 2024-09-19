e878d5ad60740c5302cb11cd1f712d1df66f87cd
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2024-09-19 11:55:38 -0400

    Fix tidy

--- hsurf/site_selection_strategy/site_selection_algorithms/steady_try_full_algo/_criteria/_calc_steady_criterion_upper_bound.py
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_try_full_algo/_criteria/_calc_steady_criterion_upper_bound.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 from .....pylib import bit_floor
 from ....site_selection_bounds import calc_gap_size_lower_bound
 
@@ -12,10 +10,13 @@ def calc_steady_criterion_upper_bound(
     if num_ingests < surface_size:
         return 0
 
-    res = 2 * bit_floor(num_ingests // surface_size)  - 1
+    res = 2 * bit_floor(num_ingests // surface_size) - 1
 
-    assert res <= (
-        2 * (surface_size + 1) / surface_size
-    ) * calc_gap_size_lower_bound(surface_size, num_ingests) + 1
+    assert (
+        res
+        <= (2 * (surface_size + 1) / surface_size)
+        * calc_gap_size_lower_bound(surface_size, num_ingests)
+        + 1
+    )
 
     return res
