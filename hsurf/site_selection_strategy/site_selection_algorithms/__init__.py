6bbddb801684a3b685982f6f4872d93568abe44a
mmore500 <mmore500.login@gmail.com>
2025-07-05 23:49:44 +0000

    Deploying to binder-coverage-criteria.ipynb from @ mmore500/hstrat-surface-concept@d0c6b6aba4be96bb40f0f7a671eb7ac0cc056e23 🚀

---
+++ hsurf/site_selection_strategy/site_selection_algorithms/__init__.py
@@ -0,0 +1,23 @@
+from . import (
+    hybrid_algo,
+    steady_algo,
+    steady_try_algo,
+    steady_try_full_algo,
+    stretched_algo,
+    stretched_try_algo,
+    tilted_algo,
+    tilted_hadamard_order_site_rungs_algo,
+    tilted_sticky_algo,
+)
+
+__all__ = [
+    "hybrid_algo",
+    "steady_algo",
+    "steady_try_algo",
+    "steady_try_full_algo",
+    "stretched_algo",
+    "stretched_try_algo",
+    "tilted_algo",
+    "tilted_sticky_algo",
+    "tilted_hadamard_order_site_rungs_algo",
+]
---
+++ hsurf/site_selection_strategy/site_selection_algorithms/steady_try_algo/_enact/_pick_ingest_site.py
@@ -0,0 +1,37 @@
+import warnings
+
+from .....pylib.hanoi import (
+    get_hanoi_value_at_index,
+    get_hanoi_value_incidence_at_index,
+)
+from ...steady_algo._impl import get_nth_bin_position, get_nth_bin_width
+from ...steady_algo._meta import has_ingest_capacity
+
+
+def pick_ingest_site(rank: int, surface_size: int) -> int:
+    """Returns surface_size if the item is to be discarded."""
+    if not has_ingest_capacity(surface_size, rank + 1):
+        warnings.warn(
+            "Rank exceeds ingest capacity. Either surface size is not "
+            "supported or too many ingestions have elapsed.",
+        )
+    assert surface_size.bit_count() == 1  # assume power of 2 surface size
+    # because 0 is special-cased for preservation...
+    assert surface_size > 1  # ... need at least somewhere to put ingests
+
+    hanoi_value = get_hanoi_value_at_index(rank)
+    hanoi_incidence = get_hanoi_value_incidence_at_index(rank)
+
+    bin_index = hanoi_incidence  # zero indexed
+    num_available_bins = surface_size // 2
+    if bin_index >= num_available_bins:
+        return surface_size
+    else:
+        bin_width = get_nth_bin_width(bin_index, surface_size)
+        within_bin_position = hanoi_value % bin_width
+
+        bin_position = get_nth_bin_position(bin_index, surface_size)
+
+        res = 1 + bin_position + within_bin_position  # index 0 reserved
+        assert 1 <= res < surface_size
+        return res
